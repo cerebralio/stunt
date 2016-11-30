308a9540088627fafff1d2bc3daf1f2eb6c57336
Alex Scheel Meyer <alexscheelmeyer@gmail.com>
2016-11-30 22:44:46 +0100

    a few more features added

--- stunt.js
+++ stunt.js
@@ -1,9 +1,11 @@
 const _ = require('lodash');
 
-function mockFunction() {
-  switch (this.action) {
+function _mock() {
+  this.called = true;
+  this.callCount++;
+  switch (this._action) {
     case 'return':
-      return this.returnValue;
+      return this._returnValue;
     default:
   }
 
@@ -11,40 +13,94 @@ function mockFunction() {
 }
 
 function yields(...args) {
-  this.action = 'yield';
-  this.yieldArgs = args;
-  return this.mockFunction;
+  this._action = 'yield';
+  this._yieldArgs = args;
+  return this._mock;
 }
 
 function returns(value) {
-  this.returnValue = value;
-  return this.mockFunction;
+  this._returnValue = value;
+  return this._mock;
 }
 
 function makeContext() {
   const context = {
-    action: 'return',
-    returnValue: 'default',
+    _action: 'return',
+    _returnValue: 'default',
+    _yieldArgs: [],
+    _mock,
+    callCount: 0,
+    called: false,
     yields,
     returns,
-    mockFunction,
   };
 
-  return _.bindAll(context, ['yields', 'returns', 'mockFunction']);
+  return context;
+}
+
+function mapContext(context) {
+  // bind all functions to the context
+  _.each(context, (value, key) => {
+    if (_.isFunction(value)) {
+      context[key] = value.bind(context);
+    }
+  });
+
+  // mirror public properties on mock-function
+  const mock = context._mock;
+  _.each(context, (value, key) => {
+    if (key[0] === '_') return; // private property
+
+    if (_.isFunction(value)) {
+      mock[key] = value;
+    } else {
+      Object.defineProperty(mock, key, { get() { return context[key]; } });
+    }
+  });
+  return mock;
 }
 
 class Stunt {
   constructor() {
-    this.mockState = [];
+    this.mockings = [];
   }
 
   function() {
     const context = makeContext();
-    const mock = context.mockFunction;
-    this.mockState.push(mock);
-    mock.yields = context.yields;
-    mock.returns = context.returns;
-    return mock;
+    return mapContext(context);
+  }
+
+  object(obj) {
+    const self = this;
+    return _.mapValues(obj, (value) => {
+      if (_.isFunction(value)) {
+        return self.function(value);
+      }
+
+      return _.cloneDeep(value);
+    });
+  }
+
+  mock(obj, property, func) {
+    const oldValue = obj[property];
+    const newFunc = func !== undefined ? func : this.function();
+    this.mockings.push({ obj, property, oldValue });
+    obj[property] = newFunc;
+  }
+
+  reset(snapshot) {
+    if (!_.isInteger(snapshot)) snapshot = 0;
+
+    for (let i = snapshot; i < this.mockings.length; i++) {
+      const { obj, property, oldValue } = this.mockings[i];
+      obj[property] = oldValue;
+    }
+
+    this.mockings.length = snapshot;
+  }
+
+  snapshot() {
+    return this.mockings.length;
   }
 }
 
