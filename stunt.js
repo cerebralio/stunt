2adc507fb39813eb086563311d0a29d6d8460d7c
Alex Scheel Meyer <alexscheelmeyer@gmail.com>
2016-12-05 21:57:55 +0100

    some more features, and testing

--- stunt.js
+++ stunt.js
@@ -3,19 +3,45 @@ const _ = require('lodash');
 function _mock(...args) {
   this.called = true;
   this.callCount++;
+  // eslint-disable-next-line default-case
   switch (this._action) {
     case 'return':
       return this._returnValue;
     case 'yield': {
       const callback = _.find(args, _.isFunction);
-      if (callback) callback(...this._yieldArgs);
+      if (!callback) throw new Error('expected to yield, but no callback given');
 
+      callback(...this._yieldArgs);
       break;
     }
-    default:
+    case 'invoke':
+      args[this._argIndex](...this._yieldArgs);
+      break;
+    case 'call': {
+      const target = this._yieldArgs[0];
+      return target(...args);
+    }
   }
 
   return void 0; // eslint-disable-line no-void
+}
+
+function invokes(argIndex, args) {
+  if (!_.isNumber(argIndex)) throw new Error('argument index must be a number');
+  if (!_.isArray(args)) throw new Error('needs arguments for invokation as array');
+
+  this._action = 'invoke';
+  this._argIndex = argIndex;
+  this._yieldArgs = args;
+  return this._mock;
+}
+
+function calls(target) {
+  if (!_.isFunction(target)) throw new Error('target must be a function');
+
+  this._action = 'call';
+  this._yieldArgs = [target];
+  return this._mock;
 }
 
 function yields(...args) {
@@ -32,13 +58,16 @@ function returns(value) {
 function makeContext() {
   const context = {
     _action: 'return',
-    _returnValue: 'default',
+    _returnValue: void 0, // eslint-disable-line no-void
+    _argIndex: 0,
     _yieldArgs: [],
     _mock,
     callCount: 0,
     called: false,
     yields,
     returns,
+    invokes,
+    calls,
   };
 
   return context;
@@ -69,11 +98,17 @@ function mapContext(context) {
 class Stunt {
   constructor() {
     this.mockings = [];
+
+    this.Stunt = Stunt; // allow circumventing the default behavior of providing a shared instance.
   }
 
   function() {
     const context = makeContext();
     return mapContext(context);
+  }
+
+  spy(target) {
+    return this.function().calls(target);
   }
 
   object(obj) {
@@ -87,11 +122,21 @@ class Stunt {
     });
   }
 
-  mock(obj, property, func) {
+  replace(obj, property, func) {
     const oldValue = obj[property];
     const newFunc = func !== undefined ? func : this.function();
     this.mockings.push({ obj, property, oldValue });
     obj[property] = newFunc;
+  }
+
+  spyOn(obj, property) {
+    const oldValue = obj[property];
+    if (!_.isFunction(oldValue)) throw new Error('property must be a function');
+
+    this.mockings.push({ obj, property, oldValue });
+
+    obj[property] = this.spy(oldValue);
+    return obj[property];
   }
 
   reset(snapshot) {
---
+++ test/stunt.js
@@ -0,0 +1,145 @@
+const _ = require('lodash');
+const { Stunt } = require('../stunt');
+const { expect } = require('chai');
+
+describe('Stunt', () => {
+  let stunt;
+  beforeEach(() => {
+    stunt = new Stunt();
+  });
+
+  afterEach(() => {
+  });
+
+  it('should allow mocking a function to return a value', () => {
+    const values = [42, 'habla espanol?', 3.14, true, () => false];
+
+    _.each(values, (value) => {
+      const mock = stunt.function().returns(value);
+      expect(mock()).to.equal(value);
+      expect(mock.called).to.be.true;
+      expect(mock.callCount).to.equal(1);
+    });
+  });
+
+  it('should allow mocking a function to yield a value', (done) => {
+    const values = [42, 'habla espanol?', 3.14, true, () => false];
+
+    function yielder(index) {
+      if (index >= values.length) {
+        done();
+        return;
+      }
+
+      const value = values[index];
+      const mock = stunt.function().yields(value);
+      mock((val) => {
+        expect(val).to.equal(value);
+        yielder(index + 1);
+      });
+    }
+
+    yielder(0);
+  });
+
+  it('should fail when a yielding mock is given no callback', () => {
+    expect(() => (stunt.function().yields(42))()).to.throw();
+  });
+
+  it('should allow mocking a function to invoke a parameter', (done) => {
+    const mock = stunt.function().invokes(2, ['hello invokee']);
+    mock('a', 42, (str) => {
+      expect(str).to.equal('hello invokee');
+      done();
+    });
+  });
+
+  it('should fail when trying to invoke with non-number for parameter index', () => {
+    expect(() => stunt.function().invokes(null, [])).to.throw();
+  });
+
+  it('should fail when trying to invoke with non-array for invokee arguments', () => {
+    expect(() => stunt.function().invokes(42, 42)).to.throw();
+  });
+
+  it('should yield to first function found', (done) => {
+    const mock = stunt.function().yields(42);
+    mock(1, 'a', 3.14, (val) => {
+      expect(val).to.equal(42);
+      done();
+    });
+  });
+
+  it('should allow spying on a function', () => {
+    const spy = stunt.spy(() => 'spied on');
+    expect(spy()).to.equal('spied on');
+    expect(spy.called).to.be.true;
+    expect(spy.callCount).to.equal(1);
+  });
+
+  it('should fail creating spy when argument is not a function', () => {
+    expect(() => stunt.spy(42)).to.throw();
+  });
+
+  it('should allow mocking an object', () => {
+    const obj = { a: 42, b: () => 'hello world', c: true };
+    const mock = stunt.object(obj);
+
+    expect(mock).to.not.equal(obj);       // should make a new instance
+    expect(mock.a).to.equal(obj.a);       // should make getters
+    expect(mock.b()).to.equal(undefined); // the mocked function always returns undefined by default
+  });
+
+  it('should allow replacing an object property', () => {
+    const obj = { a: () => 'hello world' };
+    stunt.replace(obj, 'a', stunt.function().returns('replaced'));
+    expect(obj.a()).to.equal('replaced');
+  });
+
+  it('should use default mock if not function provided as replacement', () => {
+    const obj = { a: () => 'hello world' };
+    stunt.replace(obj, 'a');
+    expect(obj.a()).to.equal(undefined);
+  });
+
+  it('should allow spying on an object property', () => {
+    const obj = { a: () => 'hello world' };
+    const spy = stunt.spyOn(obj, 'a');
+    expect(spy.called).to.be.false;
+    expect(spy.callCount).to.equal(0);
+    expect(obj.a()).to.equal('hello world');
+    expect(spy.called).to.be.true;
+    expect(spy.callCount).to.equal(1);
+  });
+
+  it('should fail when spying on non-function properties', () => {
+    const obj = { a: 42 };
+    expect(() => stunt.spyOn(obj, 'a')).to.throw();
+  });
+
+  it('should allow resetting what was replaced', () => {
+    const obj = { a: () => 'to be replaced' };
+    stunt.replace(obj, 'a', stunt.function().returns('replaced'));
+    expect(obj.a()).to.equal('replaced');
+    stunt.reset();
+    expect(obj.a()).to.equal('to be replaced');
+  });
+
+  it('should allow resetting to a snapshot in time', () => {
+    const obj = { a: () => 'to be replaced', b: () => 'to be spied on' };
+    stunt.replace(obj, 'a', stunt.function().returns('replaced'));
+    expect(obj.a()).to.equal('replaced');
+
+    const snapshot = stunt.snapshot();
+    const spy = stunt.spyOn(obj, 'b');
+    expect(obj.b()).to.equal('to be spied on');
+    expect(spy.called).to.be.true;
+    expect(spy.callCount).to.equal(1);
+
+    stunt.reset(snapshot);
+    expect(obj.a()).to.equal('replaced');
+    expect(obj.b()).to.equal('to be spied on');
+    expect(spy.called).to.be.true;
+    expect(spy.callCount).to.equal(1);
+  });
+});
